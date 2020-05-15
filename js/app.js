370786e81d5ae1b24bfb485580a13eea5befc52e
alex-whan <whan.alexander@gmail.com>
2020-05-14 19:00:55 -0700

    Added code for object prototypes addItem and saveToLocalStorage

--- js/app.js
+++ js/app.js
@@ -8,15 +8,23 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  var addedCartItem = new CartItem(product, quantity);
+  this.items.push(addedCartItem);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+
+  var stringifiedCart = JSON.stringify(this.items);
+  localStorage.setItem('cart', stringifiedCart);
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+
+
+  
 };
 
 var CartItem = function(product, quantity) {
